1aff8ed28f1fed3aafd2df99b1d8f38235f92485
Alexey Kostin <rumanzo@yandex.ru>
2022-04-16 20:50:37 +0300

    get files vrom both v1 and v2 torrents

---
+++ pkg/torrentStructures/functions.go
@@ -0,0 +1,41 @@
+package torrentStructures
+
+import (
+	"github.com/rumanzo/bt2qbt/pkg/fileHelpers"
+)
+
+func (t *Torrent) GetFileList() []string {
+	var files []string
+	if t.Info.Files != nil {
+		for _, file := range t.Info.Files {
+			if file.PathUTF8 != nil {
+				files = append(files, fileHelpers.Join(file.PathUTF8, "/"))
+			}
+		}
+	} else { // torrent v2 with FileTree
+		files = getFile(t.Info.FileTree)
+	}
+	return files
+}
+
+func getFile(f interface{}) []string {
+	nfiles := []string{}
+
+	for k, v := range f.(map[string]interface{}) {
+		if len(k) == 0 { // it's means that next will be structure with length and piece root
+			return nfiles
+		}
+
+		s := getFile(v)
+
+		if len(s) > 0 {
+			for _, path := range s {
+				nfiles = append(nfiles, fileHelpers.Join(append([]string{k}, path), "/"))
+			}
+		} else { // it's mean it was last node, juse return key
+			nfiles = append(nfiles, k)
+		}
+	}
+
+	return nfiles
+}
